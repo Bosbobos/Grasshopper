fbb87d14818f7c6d296c32340f45a647849caa69
Matvey Konovalov <konovalov.matvej@mail.ru>
2025-02-27 16:10:56 +0300

    Added keyManager

---
+++ KeyManager.py
@@ -0,0 +1,42 @@
+from OperationManager import OperationManager
+from Tools import *
+
+class KeyManager:
+    opManager = OperationManager()
+    key = ''
+    round_const = ['0'*128]
+    k = ['0' * 128] * 11 #round keys
+
+    def __init__(self, key):
+        if len(key) != 256:
+            raise Exception('Key length must be 256')
+
+        self.key = key
+        self.calculate_round_consts()
+        self.calculate_round_keys()
+
+    def calculate_round_consts(self):
+        for i in range(1, 32+1):
+            vc = vec128(i)
+            self.round_const.append(self.opManager.linear_transform(vc))
+
+    def F(self, k, a1, a0):
+        res = xor(a1, k)
+        res = self.opManager.nonlinear_transform(res)
+        res = self.opManager.linear_transform(res)
+        res = xor(res, a0)
+
+        return (res, a1)
+
+    def calculate_round_keys(self):
+        key = self.key[::-1]
+        self.k[0], self.k[1] = key[:128], key[128:]
+
+        for i in range(1, 4+1):
+            k1, k2 = self.k[2 * i - 1], self.k[2 * i]
+            for j in range(1, 8+1):
+                 k1, k2 = (
+                    self.F(self.round_const[8*(i-1)+j],
+                    k1, k2))
+
+            self.k[2 * i + 1], self.k[2 * i + 2] = k1, k2
--- Tools.py
+++ Tools.py
@@ -4,6 +4,9 @@ def int8(vec):
 def vec8(num):
     return bin(num)[2:].zfill(8)
 
+def vec128(num):
+    return bin(num)[2:].zfill(128)
+
 def reverse_bytes(vec):
     res = []
     for i in range(0, len(vec), 8):
@@ -11,3 +14,7 @@ def reverse_bytes(vec):
         res.append(block)
 
     return ''.join(res[::-1])
+
+_xormap = {('0', '1'): '1', ('1', '0'): '1', ('1', '1'): '0', ('0', '0'): '0'}
+def xor(x, y):
+    return ''.join([_xormap[a, b] for a, b in zip(x, y)]).zfill(len(x))
